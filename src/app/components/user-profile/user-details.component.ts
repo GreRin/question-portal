9d81700f73d6ec1b609c6456912e5228fec39b0e
Ryhor Sidoryn <rigor1988@gmail.com>
2020-04-19 00:38:44 +0300

    feat: finish alone task

--- src/app/components/user-profile/user-details.component.ts
+++ src/app/components/user-profile/user-details.component.ts
@@ -8,6 +8,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
+
 export class UserDetailsComponent implements OnInit {
 
   constructor(
